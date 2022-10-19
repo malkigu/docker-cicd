65376b562146600e8c860194657cf67b58ddd1fd
malkigu <86559714+malkigu@users.noreply.github.com>
2022-10-19 11:44:18 +0300

    Update job2.groovy

--- basics/dsl/job2.groovy
+++ basics/dsl/job2.groovy
@@ -16,3 +16,22 @@ job('NodeJS example') {
         shell("npm install")
     }
 }
+
+job('NodeJS example') {
+    scm {
+        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+            node / gitConfigName('DSL yaniv')
+            node / gitConfigEmail('jenkins-dsl@newtech.academy')
+        }
+    }
+    triggers {
+        scm('H/5 * * * *')
+    }
+    wrappers {
+        nodejs('nodejs') // this is the name of the NodeJS installation in 
+                         // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
+    }
+    steps {
+        shell("docker run -it alpine echo hi!!")
+    }
+}
