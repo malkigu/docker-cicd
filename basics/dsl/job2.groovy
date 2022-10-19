ae9242956ce3d49c0f7c3292582ebc5712485801
malkigu <86559714+malkigu@users.noreply.github.com>
2022-10-19 11:33:26 +0300

    Update job2.groovy

--- basics/dsl/job2.groovy
+++ basics/dsl/job2.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://github.com/malkigu/docker-cicd.git') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL yaniv')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
